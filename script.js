b4e2e20555506b4adc3851b1087d1d10fccbb230
Adrian1706 <adrianjbuenop@gmail.com>
2023-05-25 17:25:37 -0500

    feat: :rocket: casi terminado el 3
    Faltan ciertos detalles por terminar

--- script.js
+++ script.js
@@ -1,29 +1,66 @@
-class animal{
-    constructor(nombre, edad){
-        this.nombre=nombre
-        this.edad=edad
+class figura{
+    constructor(color,area){
+        this.color = color;
+        this.area = area;
     }
-    hacerSonido(){
-        return this.nombre + " está haciendo un sonido"
-    }
-}
-class perro extends animal {
-    constructor(nombre, edad, raza){
-        super(nombre, edad)
-        this.raza=raza
-    }
-    moverCola(){
-        return this.nombre + " de raza " + this.raza + " está moviendo la cola"
+    calcularArea(){
+        return this.area;
     }
 }
-const boton=document.querySelector("#sound");
+class circulo extends figura{
+    constructor(color, area, radio){
+        super(color,area);
+        this.radio = radio;
+    }
+    calcularArea(){
+        return "El area del circulo es" + Math.PI*this.radio*this.radio;
+    }
+}
+class rectangulo extends figura{
+    constructor(color, area, largo, ancho){
+        super(color,area);
+        this.largo = largo;
+        this.ancho = ancho;
+    }
+    calcularArea(){
+        return "El area del rectangulo es " + this.largo*this.ancho;
+    }
+}
+seleccionar = document.querySelector("#selec")
+seleccionar.addEventListener("change", function(e){
+    e.preventDefault()
+    let selec = document.querySelector("#selec").value;
+    if(selec=="circulo"){
+        document.querySelector("#new").innerHTML=`
+            <div class="col-12">
+                <input class="form-control me-2" type="text" placeholder="Ingrese el radio" id="radio" style="text-align: center; margin-top: 10px;">
+            </div>`
+    }else if(selec=="rectangulo"){
+        document.querySelector("#new").innerHTML=`
+        <div class="col-6">
+            <input class="form-control me-2" type="text" placeholder="Ingrese el largo" id="largo" style="text-align: center; margin-top: 10px;">
+        </div>
+        <div class="col-6">
+            <input class="form-control me-2" type="text" placeholder="Ingrese el ancho" id="ancho" style="text-align: center; margin-top: 10px;">
+        </div>`
+    }
+})
+const boton=document.querySelector("#calculo");
 boton.addEventListener("click",function(e){
     e.preventDefault()
-    let nombre=document.querySelector("#name").value;
-    let edad=document.querySelector("#age").value;
-    let raza=document.querySelector("#raza").value;
-    let pet = new perro(nombre, edad, raza); 
-    document.querySelector("#sonido").innerHTML=`
-    <p style="background-color:white; margin-top: 20px; margin-left:200px; width:200px; text-align:center">${pet.hacerSonido()}</p>
-    <p style="background-color:white; margin-left:200px; width:200px; text-align:center">${pet.moverCola()}</p>`
-});
+    let color = document.querySelector("#color").value;
+    let area = document.querySelector("#area").value;
+    
+    let largo = document.querySelector("#largo").value;
+        let ancho = document.querySelector("#ancho").value;
+    let obj = new circulo(color, area, radio);
+    let obj2 = new rectangulo(color, area, largo, ancho);
+    if (select=="circulo"){
+        document.querySelector("#result").innerHTML=`
+        <p style="background-color:white; margin-top: 20px; margin-left:200px; width:200px; text-align:center">El radio del circulo es ${obj.calcularArea()}</p>`
+    }
+    else if (select=="rectangulo"){
+        document.querySelector("#result").innerHTML=`
+                <p style="background-color:white; margin-top: 20px; margin-left:200px; width:200px; text-align:center">El largo del rectangulo es ${obj2.calcularArea()}</p>`
+    }
+})
